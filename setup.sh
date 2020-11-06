63e680ecc30f93335e8455d0ef284c67c8f33732
ZhiQiu <qziuhi@126.com>
2020-11-06 09:55:36 -0500

    update

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"zhi.qiu731@duke.edu\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+\n\
 " > ~/.streamlit/config.toml
